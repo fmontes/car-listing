f73e02b5a2de19a6972310ff9c61f4103489ced0
Freddy Montes <freddymontes@gmail.com>
2018-08-25 06:32:31 -0600

    Create header and listing component and setup routing

---
+++ src/app/pages/list/list.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Routes, RouterModule } from '@angular/router';
+
+import { ListComponent } from './list.component';
+
+const routes: Routes = [
+    {
+        component: ListComponent,
+        path: ''
+    }
+];
+
+@NgModule({
+    imports: [
+        CommonModule,
+        RouterModule.forChild(routes),
+    ],
+    declarations: [ListComponent]
+})
+export class ListModule {}
