754c1e35439a3770217f73c98e6a3a18e8cb9ad3
Freddy Montes <freddymontes@gmail.com>
2018-08-25 06:01:05 -0600

    Add and run prettier

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'car-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'car-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'car-listing';
+    title = 'car-listing';
 }
