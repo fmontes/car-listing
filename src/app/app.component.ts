afc5a048ce4354e0c9b1ae919859fcd4b5237e5c
Freddy Montes <freddymontes@gmail.com>
2018-11-25 10:04:42 -0600

    Add route animation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,31 @@
 import { Component } from '@angular/core';
 
+import {
+    trigger,
+    animate,
+    transition,
+    style,
+    query
+} from '@angular/animations';
+
+export const fadeAnimation = trigger('fadeAnimation', [
+    transition('* => *', [
+        query(':enter', [style({ opacity: 0 })], { optional: true }),
+        query(
+            ':leave',
+            [style({ opacity: 1 }), animate('0.3s', style({ opacity: 0 }))],
+            { optional: true }
+        ),
+        query(
+            ':enter',
+            [style({ opacity: 0 }), animate('0.3s', style({ opacity: 1 }))],
+            { optional: true }
+        )
+    ])
+]);
+
 @Component({
+    animations: [fadeAnimation],
     selector: 'car-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss']
